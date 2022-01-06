af9c52aabce438e51f55e054c28c815911902449
tom-sharp <dink521g@gmail.com>
2022-01-07 00:10:17 +0100

    initial version 0.01

---
+++ SudokuWebAPI/Controllers/SudokuController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Sudoku.Puzzle;
+using SudokuWebAPI.Dtos;
+
+/*
+
+	A web api to create or solve sudoku puzzles.
+	(Dependent on the SudokuPuzzle assembly)
+
+	http(s)://server/api/sudoku
+		will generate a random sudoku puzzle and return it as a json object with:
+			status = indicate that all went well and should be "Ok"
+			puzzle = is the puzzle string
+	
+	http(s)://server/api/sudoku/{puzzle}
+		will solve sudoku {puzzle} and return a json object with:
+			status = indicate that all went well and should be "Ok" if solved
+			puzzle = is the solved puzzle string
+
+	ver		description
+	0.01	initial
+ 
+ */
+
+
+
+namespace SudokuWebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class SudokuController : ControllerBase
+	{
+		// GET: api/<SudokuController>
+		[HttpGet]
+		public ActionResult<PuzzleDto> Get() {
+			return Ok(new PuzzleDto("Ok", new CreateSudoku().GetSudokuPuzzle().GetPuzzle()));
+		}
+
+		// GET api/<SudokuController>/5
+		[HttpGet("{puzzle}")]
+		public ActionResult<PuzzleDto> Get(string puzzle) {
+			SudokuPuzzle sudoku = new SudokuPuzzle(puzzle);
+			if (!sudoku.IsValid()) return Ok(new PuzzleDto("Invalid", sudoku.GetPuzzle()));
+			sudoku.ResolveRules();
+			if (!sudoku.IsValid()) return Ok(new PuzzleDto("Invalid", sudoku.GetPuzzle()));
+			if (!sudoku.IsSolved()) sudoku.ResolveBacktrack();
+			if (sudoku.IsSolved()) return Ok(new PuzzleDto("solved", sudoku.GetPuzzle()));
+			return Ok(new PuzzleDto("Invalid", sudoku.GetPuzzle()));
+		}
+
+	}
+}
---
+++ SudokuWebAPI/Dtos/PuzzleDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SudokuWebAPI.Dtos
+{
+	public class PuzzleDto
+	{
+		public PuzzleDto(string status, string puzzle) { this.Status = status; this.Puzzle = puzzle; }
+		public string Status { get; set; }
+		public string Puzzle { get; set; }
+	}
+
+}
